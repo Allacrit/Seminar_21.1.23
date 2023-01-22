32a31a5b7440a5916fe6d6ea3d6023a53e9a67dc
Artem Oleynikov <allacrit@yandex.ru>
2023-01-22 20:25:00 +0300

    Создание решения задачи №3

---
+++ 03/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("3. Возведите число А в натуральную степень B используя цикл.");
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Введите степень: ");
+int exponent = int.Parse(Console.ReadLine());
+
+int count = 1;
+int result = number;
+
+while (count < exponent)
+{
+    result = result * number;
+    count++;
+}
+
+Console.WriteLine($"{number} ^ {exponent} = {result}");
+Console.WriteLine();
