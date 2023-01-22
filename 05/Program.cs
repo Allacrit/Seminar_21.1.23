3f59b36e8dfc379dd6bf3eb5f3c37b004ad1761c
Artem Oleynikov <allacrit@yandex.ru>
2023-01-22 20:40:57 +0300

    Создание решения задачи №5

---
+++ 05/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine();
+Console.Write("Введите число: ");
+string writeN = Console.ReadLine();
+int number = int.Parse(writeN);
+
+Console.WriteLine($"Произведение чисел от 1 до {number} равно {GetNumbersComposition(number)}.");
+
+int GetNumbersComposition(int x)
+{
+    int comp = 1;
+    for (int num = 1; num <= x; num++)
+    {
+        comp = comp * num;
+    }
+    return comp;
+}
