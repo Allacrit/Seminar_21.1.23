42e3b32d2a2604fba2356f8647761f9031c6a2a8
Artem Oleynikov <allacrit@yandex.ru>
2023-01-22 21:20:23 +0300

    Создание решения задачи №6

---
+++ 06/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("6. Показать кубы чисел, заканчивающихся на четную цифру.");
+Console.Write("Введите число до которого будут производится вычисления: ");
+int numberN = int.Parse(Console.ReadLine());
+
+int number = 1;
+while (number <= numberN)
+{
+    int cube = number * number * number;
+    if (cube % 2 == 0) Console.WriteLine($"Число {number} в кубе равно {cube}.");
+    number++;
+}
