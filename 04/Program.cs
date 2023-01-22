63d8c3c2b62c59cec40ecbd153ef710a0aad1e45
Artem Oleynikov <allacrit@yandex.ru>
2023-01-22 20:34:33 +0300

    Создание решения задачи №4

---
+++ 04/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("4. Подсчитать сумму цифр в числе.");
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int numberClone = number;
+int sumDigit = 0;
+
+while (numberClone > 0)
+{
+    int digit = numberClone % 10;
+    sumDigit = sumDigit + digit;
+    numberClone = numberClone / 10;
+}
+
+Console.WriteLine($"Сумма цифр числа {number} равна {sumDigit}.");
